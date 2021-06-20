d37b08984928b1b54d9e92fcb1b88c15ff6a3ca0
adichter-hpe <andreas.dichter@hpe.com>
2021-06-20 17:26:09 +0200

    webapp movie selection

--- app/app.py
+++ app/app.py
@@ -11,21 +11,47 @@ app = Flask(__name__)
 #keywords = pd.read_csv (r'./../data/keywords.csv')
 #links = pd.read_csv (r'./../data/links.csv')
 #links_small = pd.read_csv (r'./../data/links_small.csv')
-movies_metadata = pd.read_csv (r'./../data/movies_metadata.csv')
-
+movies_metadata = pd.read_csv (r'./movies_metadata.csv')
+global movies_like
+movies_like = []
 # Home
 @app.route('/')
+
 def home():
     #choosing 10 random movieIds from collection
+    global movies_like
     random_ids = []
     for i in range(10):
         random_ids.append(random.choice(movies_metadata.loc[:,'id'].array))
     
-    movie_selection = []
-    for i in random_ids:
-        movie_selection.append(movies_metadata.loc[movies_metadata['id'] == str(i), 'original_title'].array[0])   
+    def get_title(id):
+        return movies_metadata.loc[movies_metadata['id'] == str(id), 'title'].array[0]
+    def get_date(id):
+        return movies_metadata.loc[movies_metadata['id'] == str(id), 'release_date'].array[0]
+    def like(id):
+        movies_like.append(id)
+        print(id, movies_like)
+        return ""
+    
     return render_template("home.html", 
-                            movie_selection=movie_selection)
+                            random_ids = random_ids,
+                            get_title = get_title,
+                            get_date = get_date,
+                            like = like,
+                            movies_like = movies_like)
+
+#def picked_movies():
+#    if request.form.get("like"):
+#        movies_like.append(id)
+#
+#    
+#    return render_template("home.html", 
+#                            random_ids = random_ids,
+#                            get_title = get_title,
+#                            get_date = get_date,
+#                            like = like,
+#                            movies_like = movies_like)
+#
 
 
 if __name__ == '__main__':
